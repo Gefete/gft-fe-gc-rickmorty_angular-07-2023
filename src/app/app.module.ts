49515c0c17940822fc0cf70c3e6fc5d82dfce9af
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-11 19:11:45 +0200

    complete exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { CharactersComponent } from './home/characters/characters.component';
+import { AboutComponent } from './home/about/about.component';
+import { DetalleComponent } from './home/characters/detalle/detalle.component';
+import { BienvenidaComponent } from './home/bienvenida/bienvenida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    CharactersComponent,
+    AboutComponent,
+    DetalleComponent,
+    BienvenidaComponent
   ],
   imports: [
     BrowserModule,
